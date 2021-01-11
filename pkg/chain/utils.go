6f87e3c80269386e07dc12ae1fa590c3d496fc12
MarsOccupied <mirzaei.Seppi@gmail.com>
2021-01-11 23:07:31 +0100

    fix formatting

---
+++ pkg/chain/utils.go
@@ -0,0 +1,41 @@
+package chain
+
+import (
+	"github.com/MarsOccupied/plasma-core/plasma/pkg/chain"
+	"github.com/MarsOccupied/plasma-core/plasma/util"
+	"github.com/MarsOccupied/plasma-core/plasma/pkg/rpc/pb"
+	"github.com/pkg/errors"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+import (
+	"math/big"
+	"sync"
+)
+
+var zero, one *big.Int
+var exitOutput *Output
+var once sync.Once
+
+func initialize() {
+	zero = big.NewInt(0)
+	one  = big.NewInt(1)
+	exitOutput = ZeroOutput()
+	exitOutput.Owner = common.HexToAddress("0xDeadBeefCafe")
+}
+
+func Zero() *big.Int {
+	once.Do(initialize)
+	return zero
+}
+
+func One() *big.Int {
+	once.Do(initialize)
+	return one
+}
+
+func ExitOutput() *Output {
+	once.Do(initialize)
+	return exitOutput
+}
+
