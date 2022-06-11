c64bf38e2ef4b4299e98093131629ecfab35f0f1
MarsOccupied <mirzaei.Seppi@gmail.com>
2022-06-12 00:32:25 +0200

    fix goroutine leak

---
+++ pkg/eth/signatures.go
@@ -0,0 +1,38 @@
+package eth
+
+import (
+	"crypto/ecdsa"
+	"errors"
+	)
+
+func Sign(privKey *ecdsa.PrivateKey, hash util.Hash) (chain.Signature, error) {
+	ethHash := util.GethHash(hash)
+	var sig chain.Signature
+	rawSig, err := crypto.Sign(ethHash, privKey)
+	if err != nil {
+		return sig, err
+	}
+	copy(sig[:], rawSig)
+	return sig, nil
+}
+
+func ValidateSignature(hash, signature []byte, address common.Address) error {
+	ethHash := util.GethHash(hash)
+
+	sigCopy := make([]byte, len(signature))
+	copy(sigCopy, signature)
+	if len(sigCopy) == 65 && sigCopy[64] > 26 {
+		sigCopy[64] -= 27
+	}
+
+	pubKey, err := crypto.SigToPub(ethHash, sigCopy)
+	if err != nil {
+		return err
+	}
+
+	signatureAddress := crypto.PubkeyToAddress(*pubKey)
+	if address != signatureAddress {
+		return errors.New("invalid signature")
+	}
+	return nil
+}
