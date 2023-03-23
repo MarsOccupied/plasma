13a2d3bbf9fa633fe3751ff6d43e037fac286865
MarsOccupied <mirzaei.Seppi@gmail.com>
2023-03-24 00:26:21 +0100

    optimize performance

---
+++ pkg/db/exit_locator.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+)
+
+type ExitLocator struct {
+	PlasmaBlockNumber       uint64
+	PlasmaTransactionIndex  uint32
+	PlasmaOutputIndex       uint8
+	EthereumBlockNumber     uint64
+	EthereumTransactionHash []byte
+}
+
+func (e *ExitLocator) MarshalBinary() ([]byte, error) {
+	return rlp.EncodeToBytes(e)
+}
+
+func (e *ExitLocator) UnmarshalBinary(data []byte) error {
+	return rlp.DecodeBytes(data, e)
+}
