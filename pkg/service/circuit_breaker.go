d9a6c70072ac989258ba656f7798dbd649ef3bf6
MarsOccupied <mirzaei.Seppi@gmail.com>
2023-07-27 20:46:12 +0200

    fix lint errors

---
+++ pkg/service/circuit_breaker.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"sync/atomic"
+)
+
+var breakerLogger = log.ForSubsystem("CircuitBreaker")
+
+type CircuitBreaker interface {
+	Tripped() bool
+	Trip()
+	Reset()
+}
+
+type CircuitBreakerImpl struct {
+	tripped uint32
+
+	logger *logrus.Entry
+}
+
+func NewCircuitBreaker(name string) CircuitBreaker {
+	return &CircuitBreakerImpl{
+		logger: breakerLogger.WithFields(logrus.Fields{
+			"name": name,
+		}),
+	}
+}
+
+func (c *CircuitBreakerImpl) Tripped() bool {
+	return atomic.LoadUint32(&c.tripped) == 1
+}
+
+func (c *CircuitBreakerImpl) Trip() {
+	c.logger.Warn("circuit breaker tripped")
+	atomic.StoreUint32(&c.tripped, 1)
+}
+
+func (c *CircuitBreakerImpl) Reset() {
+	c.logger.Warn("circuit breaker reset")
+	atomic.StoreUint32(&c.tripped, 0)
+}
