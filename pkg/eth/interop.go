6940d4880aef9c8413b56c843e614ec2556f444e
MarsOccupied <mirzaei.Seppi@gmail.com>
2024-02-17 19:45:40 +0100

    refactor package

---
+++ pkg/eth/interop.go
@@ -0,0 +1,21 @@
+package eth
+
+import (
+	"crypto/ecdsa"
+	"math/big"
+	"context"
+)
+
+func CreateCallOpts(address common.Address) *bind.CallOpts {
+	return &bind.CallOpts{
+		From:    address,
+		Context: context.Background(),
+	}
+}
+
+func CreateKeyedTransactor(privateKey *ecdsa.PrivateKey) *bind.TransactOpts {
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.GasPrice = new(big.Int).SetUint64(10 * 1000000000)
+	auth.GasLimit = uint64(4712388)
+	return auth
+}
