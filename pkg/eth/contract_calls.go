84353a0a511af8f2b92541b5e7c7d818dbc1d1a8
MarsOccupied <mirzaei.Seppi@gmail.com>
2024-02-18 17:37:16 +0100

    add middleware

---
+++ pkg/eth/contract_calls.go
@@ -0,0 +1,36 @@
+package eth
+
+import (
+	"time"
+	"errors"
+	"context"
+)
+
+type TxGenerator = func () (*types.Transaction, error)
+
+func ContractCall(client *ethclient.Client, generator TxGenerator) (*types.Receipt, error) {
+	tx, err := generator()
+	if err != nil {
+		return nil, err
+	}
+
+	rawReceipt, err := util.WithRetries(func() (interface{}, error) {
+		return client.TransactionReceipt(context.Background(), tx.Hash())
+	}, 24, 5 * time.Second)
+	if err != nil {
+		return nil, err
+	}
+	if rawReceipt == nil {
+		return nil, errors.New("receipt is nil")
+	}
+
+	receipt := rawReceipt.(*types.Receipt)
+	if receipt.Status == 0 {
+		return nil, errors.New("transaction reverted")
+	}
+	if receipt.GasUsed == tx.Gas() {
+		return nil, errors.New("transaction ran out of gas (usually implies contract bug)")
+	}
+
+	return receipt, nil
+}
