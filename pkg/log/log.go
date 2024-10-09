368a72930cbb69388a208f8c450c4d311ac186e3
MarsOccupied <mirzaei.Seppi@gmail.com>
2024-10-09 18:37:25 +0200

    refactoring code

---
+++ pkg/log/log.go
@@ -0,0 +1,18 @@
+package log
+
+
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+}
+
+func ForSubsystem(subsystem string) *logrus.Entry {
+	return logrus.WithFields(logrus.Fields{
+		"subsystem": subsystem,
+	})
+}
+
+func WithError(logger *logrus.Entry, err error) *logrus.Entry {
+	return logger.WithFields(logrus.Fields{
+		"err": err,
+	})
+}
