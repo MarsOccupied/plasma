9dc5e9190eb797c4be8f6c4a07ebe2227650a039
MarsOccupied <mirzaei.Seppi@gmail.com>
2024-10-21 11:20:37 +0200

    add middleware

---
+++ cmd/plasmad/cmd/root.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"fmt"
+	"github.com/spf13/viper"
+	"github.com/MarsOccupied/plasma-core/plasma/pkg/db"
+)
+
+var boundFlags = []string{
+	FlagDB,
+	FlagNodeURL,
+	FlagContractAddr,
+	FlagPrivateKey,
+}
+
+var configFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "plasma",
+	Short: "An implementation of the Minimum Viable Plasma spec.",
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&configFile, FlagConfig, "", "filepath to Plasma's configuration file")
+	rootCmd.PersistentFlags().String(FlagDB, db.DefaultLevelLocation(), "filepath to Plasma's database")
+	rootCmd.PersistentFlags().String(FlagNodeURL, "", "full URL to a running Ethereum node")
+	rootCmd.PersistentFlags().String(FlagContractAddr, "", "address of the Plasma contract")
+	rootCmd.PersistentFlags().String(FlagPrivateKey, "", "node operator's private key")
+	for _, flag := range boundFlags {
+		rootCmd.MarkFlagRequired(flag)
+		viper.BindPFlag(flag, rootCmd.PersistentFlags().Lookup(flag))
+	}
+}
+
+func initConfig() {
+	if configFile == "" {
+		return
+	}
+
+	viper.SetConfigFile(configFile)
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Invalid config file:", err)
+		os.Exit(1)
+	}
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
