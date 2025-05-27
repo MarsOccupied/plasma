b6dfad0e79566017ea5b99c88fddf0c9edcebb82
MarsOccupied <mirzaei.Seppi@gmail.com>
2025-05-27 12:33:22 +0200

    address comments

---
+++ integration_tests/test/Basic.test.ts
@@ -0,0 +1,11 @@
+import {toBig} from '@marsoccupied/plasma-client/lib/util/numbers';
+import IRootClient from '@marsoccupied/plasma-client/lib/rpc/IRootClient';
+import {assertBigEqual} from './lib/assertBigEqual';
+import bothClients from './lib/bothClients';
+
+bothClients((client: IRootClient) => describe('Basic functionality', () => {
+  it('should return a zero balance for a non-existent account', async () => {
+    const res = await client.getBalance('0x0000000000000000000000000000000000000000');
+    assertBigEqual(res, toBig(0));
+  });
+}));
